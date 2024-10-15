a481b961a3beb6b13b9c74d1c624688cac228d5b
MichaelOtieno <okellofrost@gmail.com>
2024-10-15 15:18:42 +0300

    finishing on profile and starting on dashboard

--- app/src/main/java/com/chinatown254/android/userinterface/IntroActivity.java
+++ app/src/main/java/com/chinatown254/android/userinterface/IntroActivity.java
@@ -1,6 +1,8 @@
 package com.chinatown254.android.userinterface;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
+public class IntroActivity extends AppCompatActivity {
+ Button gettingStarted;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_intro);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        gettingStarted = findViewById(R.id.gettingStarted);
+        gettingStarted.setOnClickListener(view -> {
+            Intent intent = new Intent(IntroActivity.this, ProfileActivity.class);
+            startActivity(intent);
         });
     }
 }
