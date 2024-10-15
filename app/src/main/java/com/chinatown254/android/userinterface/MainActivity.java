85722a40c175df7f41a40ba0737b177a34487345
MichaelOtieno <okellofrost@gmail.com>
2024-10-15 14:25:18 +0300

    adding logout button

--- app/src/main/java/com/chinatown254/android/userinterface/MainActivity.java
+++ app/src/main/java/com/chinatown254/android/userinterface/MainActivity.java
@@ -1,6 +1,8 @@
 package com.chinatown254.android.userinterface;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+ Button gettingStarted;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        gettingStarted = findViewById(R.id.gettingStarted);
+        gettingStarted.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+            startActivity(intent);
         });
     }
 }
